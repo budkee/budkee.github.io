e586a4f9148f38f440ae4476674b6d13d24ed768
budkee <camila.budke@gmail.com>
2023-12-03 17:56:58 -0400

    Configurando variáveis de ambiente

--- django_app/project_crud/settings.py
+++ django_app/project_crud/settings.py
@@ -10,22 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+## Arquivos estáticos e mídia do app
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&r#f)c$+mvr9yvvene0f==r*v%oj=n32e2&f6&j#@k5^v2i)+3"
+SECRET_KEY = os.getenv('SECRET-KEY', 'CHANGE-ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    # List comprehension: Se houver um valor, adicione o host à lista.
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h.strip()
+]
 
 
 # Application definition
@@ -75,8 +80,12 @@ WSGI_APPLICATION = "project_crud.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv('DB_ENGINE', 'CHANGE-ME'),
+        "NAME": os.getenv('POSTGRES_DB', 'CHANGE-ME'),
+        "USER": os.getenv('POSTGRES_USER', 'CHANGE-ME'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD', 'CHANGE-ME'),
+        "HOST": os.getenv('POSTGRES_HOST', 'CHANGE-ME'),
+        "PORT": os.getenv('POSTGRES_PORT', 'CHANGE-ME')
     }
 }
 
@@ -103,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "PT-BR"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -115,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+# /data/web/static
+STATIC_ROOT = DATA_DIR / "static"
+
+MEDIA_URL = "/media/"
+# /data/web/media
+MEDIA_ROOT = DATA_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
